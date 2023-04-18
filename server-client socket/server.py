ddf7b5285804bcef0af69e7c3baf73aafe4c7f58
Zehrakmnl <83715370+Zehrakmnl@users.noreply.github.com>
2023-04-19 01:39:18 +0300

    Update server.py

--- server-client socket/server.py
+++ server-client socket/server.py
@@ -1,7 +1,8 @@
 """
     Soket programlama, birbiriyle iletişim kurmak için bir ağ üzerindeki iki düğümü bağlamanın bir yoludur. 
     Bir soket (düğüm), bir IP'deki belirli bir bağlantı noktasını dinlerken, diğer soket bir bağlantı oluşturmak için diğerine ulaşır.
-    İstemci sunucuya ulaşırken sunucu dinleyici soketini oluşturur. client.py & server.py dosyalarında bu soket programlama uygulanmıştır.
+    İstemci sunucuya ulaşırken sunucu dinleyici soketini oluşturur. 
+    client.py & server.py dosyalarında bu soket programlama uygulanmıştır.
 """
 import socket
 import threading
